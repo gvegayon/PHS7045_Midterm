848a1572fa3fb489d15b0979e8f8a70fabfe8a6b
George G. Vega Yon <g.vegayon@gmail.com>
2024-11-15 17:32:32 -0700

    Update post_mu.R

--- Functions/post_mu.R
+++ Functions/post_mu.R
@@ -10,6 +10,9 @@
 #############################################################' 
 
 post_mu <- function(k, sigma, theta, b_mu){
+  # Same idea here, normal distribution can be transformed
+  # so it makes it easier to just use a single big draw of
+  # them.
   rnorm(1, 
         mean = sigma*sum(theta)/(k*sigma + 100),
         sd = 1/sqrt(k*sigma + 100))
